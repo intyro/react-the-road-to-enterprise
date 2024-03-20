9a0ecf5db043e17627cc56d203dba2ff33c22612
intyro <moath.khozyem@gmail.com>
2024-03-20 18:58:50 +0000

    add use api status hook - important

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import AnimalExample from '@/components/AnimalExample'
 import AnimalExampleWithApiStates from './components/AnimalExampleWithApiStates'
 import AnimalExampleWithApiStatesConstants from './components/AnimalExampleWithApiStatesConstants'
+import AnimalExampleWithUseApiStatus from './components/AnimalExampleWithUseApiStatus'
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
       {/* <AnimalExampleWithApiStates /> */}
-      <AnimalExampleWithApiStatesConstants />
+      {/* <AnimalExampleWithApiStatesConstants /> */}
+      <AnimalExampleWithUseApiStatus />
     </div>
   )
 }
